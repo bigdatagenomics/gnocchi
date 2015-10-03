9f2a7fd816b7a250008107d1ec60eb30c7e40313
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2015-10-03 10:06:08 -0700

    [gnocchi-8] Add basic code to fill in missing genotypes.
    This change adds a package net.fnothaft.gnocchi.imputation that contains basic
    code for filling in missing genotypes, and an accompanying command, FillIn.
    The current code support filling missing genotypes in with Ref calls or NoCall
    alleles. Resolves #8.

---
+++ gnocchi-cli/src/main/scala/net/fnothaft/gnocchi/cli/FillIn.scala
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2015 Frank Austin Nothaft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.fnothaft.gnocchi.cli
+
+import java.io.File
+import net.fnothaft.gnocchi._
+import net.fnothaft.gnocchi.avro.{ Association, Phenotype }
+import net.fnothaft.gnocchi.imputation.FillGenotypes
+import org.apache.hadoop.mapreduce.Job
+import org.apache.spark.SparkContext._
+import org.apache.spark.{ Logging, SparkContext }
+import org.bdgenomics.adam.models.ReferenceRegion
+import org.bdgenomics.adam.rdd.ADAMContext._
+import org.bdgenomics.adam.rdd.BroadcastRegionJoin
+import org.bdgenomics.formats.avro._
+import org.bdgenomics.utils.misc.HadoopUtil
+import org.bdgenomics.utils.cli._
+import org.kohsuke.args4j.{ Argument, Option => Args4jOption }
+import org.apache.parquet.avro.AvroReadSupport
+import org.apache.parquet.hadoop.ParquetInputFormat
+import org.apache.parquet.hadoop.util.ContextUtil
+
+object FillIn extends BDGCommandCompanion {
+  val commandName = "fillIn"
+  val commandDescription = "Fills in missing genotypes with ref or no call alleles."
+
+  def apply(cmdLine: Array[String]) = {
+    new FillIn(Args4j[FillInArgs](cmdLine))
+  }
+}
+
+class FillInArgs extends Args4jBase {
+  @Argument(required = true, metaVar = "INPUT", usage = "The genotypes to process.", index = 0)
+  var input: String = null
+
+  @Argument(required = true, metaVar = "OUTPUT", usage = "The processed genotypes.", index = 1)
+  var output: String = null
+
+  @Args4jOption(required = false, name = "-useNoCall",
+                usage = "Fills in genotypes with NoCall alleles, instead of Ref.")
+  var useNoCall = false
+
+  @Args4jOption(required = false, name = "-ploidy",
+                usage = "Assumed ploidy. Default is 2 (diploid).")
+  var ploidy = 2
+}
+
+class FillIn(protected val args: FillInArgs) extends BDGSparkCommand[FillInArgs] {
+  val companion = FillIn
+
+  def run(sc: SparkContext) {
+    // load in genotype data
+    val genotypes = sc.loadGenotypes(args.input)
+
+    // fill in genotypes
+    val recomputedGenotypes = FillGenotypes(genotypes,
+                                            useNoCall = args.useNoCall,
+                                            ploidy = args.ploidy)
+
+    // save to disk
+    recomputedGenotypes.adamParquetSave(args.output)
+  }
+}
---
+++ gnocchi-core/src/test/scala/net/fnothaft/gnocchi/imputation/FillGenotypesSuite.scala
@@ -0,0 +1,104 @@
+/**
+ * Copyright 2015 Frank Austin Nothaft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.fnothaft.gnocchi.imputation
+
+import net.fnothaft.gnocchi.GnocchiFunSuite
+import org.bdgenomics.adam.models.VariantContext
+import org.bdgenomics.adam.rdd.ADAMContext._
+import org.bdgenomics.formats.avro._
+import scala.collection.JavaConverters._
+
+class FillGenotypesSuite extends GnocchiFunSuite {
+
+  test("fill in a single variant context") {
+    val v = Variant.newBuilder
+      .setContig(Contig.newBuilder
+      .setContigName("1")
+      .build())
+      .setStart(1000L)
+      .setEnd(1001L)
+      .setReferenceAllele("A")
+      .setAlternateAllele("T")
+      .build()
+    val vc = VariantContext.buildFromGenotypes(Seq(Genotype.newBuilder
+      .setVariant(v)
+      .setSampleId("sample1")
+      .setAlleles(Seq(GenotypeAllele.Ref,GenotypeAllele.Alt).asJava)
+      .build()))
+
+    val newGts = FillGenotypes.fillInVC(vc,
+                                        Set("sample1", "sample2", "sample3"),
+                                        Seq(GenotypeAllele.Ref))
+    
+    assert(newGts.size === 3)
+    newGts.foreach(gt => gt.getSampleId match {
+      case "sample1" => {
+        assert(gt.getAlleles.size === 2)
+        assert(gt.getAlleles.get(0) === GenotypeAllele.Ref)
+        assert(gt.getAlleles.get(1) === GenotypeAllele.Alt)
+      }
+      case _ => {
+        assert(gt.getAlleles.size === 1)
+        assert(gt.getAlleles.get(0) === GenotypeAllele.Ref)
+      }
+    })
+    assert(newGts.map(_.getSampleId)
+      .toSeq
+      .sorted
+      .toSet === Set("sample1", "sample2", "sample3"))
+  }
+
+  sparkTest("fill in with diploid no call") {
+    val s1 = ClassLoader.getSystemClassLoader.getResource("small1.vcf").getFile
+    val s2 = ClassLoader.getSystemClassLoader.getResource("small2.vcf").getFile
+    val input = (sc.loadGenotypes(s1) ++ sc.loadGenotypes(s2)).cache
+
+    assert(input.count === 4)
+    assert(input.map(_.getVariant).distinct.count === 3)
+
+    val newGts = FillGenotypes(input,
+                               useNoCall = true).collect
+
+    assert(newGts.length === 6)
+    assert(newGts.map(_.getVariant).toSet.size === 3)
+    assert(newGts.count(_.getSampleId == "sample1") === 3)
+    assert(newGts.count(_.getSampleId == "sample2") === 3)
+    assert(newGts.map(_.getAlleles.size).forall(_ == 2))
+    assert(newGts.flatMap(_.getAlleles.asScala).count(_ == GenotypeAllele.NoCall) === 4)
+  }
+
+  sparkTest("fill in with haploid ref call") {
+    val s1 = ClassLoader.getSystemClassLoader.getResource("small1.vcf").getFile
+    val s2 = ClassLoader.getSystemClassLoader.getResource("small2.vcf").getFile
+    val input = (sc.loadGenotypes(s1) ++ sc.loadGenotypes(s2)).cache
+
+    assert(input.count === 4)
+    assert(input.map(_.getVariant).distinct.count === 3)
+
+    val newGts = FillGenotypes(input,
+                               ploidy = 1).collect
+
+    assert(newGts.length === 6)
+    assert(newGts.map(_.getVariant).toSet.size === 3)
+    assert(newGts.count(_.getSampleId == "sample1") === 3)
+    assert(newGts.count(_.getSampleId == "sample2") === 3)
+    assert(newGts.map(_.getAlleles.size).count(_ == 2) === 4)
+    assert(newGts.map(_.getAlleles.size).count(_ == 1) === 2)
+    assert(newGts.filter(_.getAlleles.size == 1)
+      .flatMap(_.getAlleles.asScala)
+      .forall(_ == GenotypeAllele.Ref))
+  }
+}
