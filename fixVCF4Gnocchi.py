0e2d246a2f3aa915914e0e7bae96548623421889
TanerDagdelen <tkdagdelen@berkeley.edu>
2016-09-23 10:47:06 -0700

    adding fixVCF4Gnocchi.py

---
+++ fixVCF4Gnocchi.py
@@ -0,0 +1,31 @@
+from tempfile import mkstemp
+from shutil import move
+from os import remove, close
+import csv
+
+
+""" Takes vcf file and sets all chromosome positions to 5"""
+def fixVCFFile(filename): 
+	fh, output_file = mkstemp()
+	with open(filename) as to_read:
+		with open(output_file, "wb") as tmp_file:
+			reader = csv.reader(to_read, delimiter = "\t")
+			writer = csv.writer(tmp_file, delimiter = " ")
+			isTrue = False
+			isfirst = True
+			for row in reader:     # read one row at a time
+				if len(row) >= 2:
+					if not row[0].startswith('#'):#'dbGaP SubjID': 
+						isTrue = True
+				if isTrue:     
+					if not isfirst: 
+						myColumn = [5, 5] + list(row[i] for i in range(2,len(row)))  
+						writer.writerow(myColumn) # write it
+					else: 
+						writer.writerow(myColumn)
+						isfirst = False
+	close(fh)
+	move(output_file,"fixed_"+filename)
+
+name = input("VCF filename:")
+fixVCFFile(name)
