3ad5a9d76ef68895fcf93c71561561f1e41f622a
TanerDagdelen <tkdagdelen@berkeley.edu>
2016-10-29 15:07:58 -0700

    more prints for debug

--- gnocchi-core/src/main/scala/net/fnothaft/gnocchi/association/LoadPhenotypesWithCovariates.scala
+++ gnocchi-core/src/main/scala/net/fnothaft/gnocchi/association/LoadPhenotypesWithCovariates.scala
@@ -201,7 +201,8 @@ private[gnocchi] object LoadPhenotypesWithCovariates extends Serializable {
         p(0), // sampleID string
         for (i <- indices) yield p(i).toDouble) // phenotype values
         .asInstanceOf[Phenotype[Array[Double]]])
-
+    println("\n\n\n\n\n\n\n\n" + fullHeader + "\n\n\n\n\n\n")
+    println("\n\n\n\n\n\n\n\n" + indices.toList + "\n\n\n\n\n")
     // unpersist the textfile
     phenotypes.unpersist()
     covars.unpersist()
