e93b27b62c6ff6d587104ec5b743991a7cb38606
TanerDagdelen <tkdagdelen@berkeley.edu>
2016-11-07 16:51:01 -0800

    implementing BuildLogisticGnocchiModel

---
+++ gnocchi-core/src/main/scala/net/fnothaft/gnocchi/gnocchiModel/BuildLogisticGnocchiModel.scala
@@ -0,0 +1,69 @@
+/**
+  * Copyright 2016 Taner Dagdelen
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package net.fnothaft.gnocchi.gnocchiModel
+
+import net.fnothaft.gnocchi.models.{GenotypeState, Phenotype, Association}
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.linalg.DenseVector
+import org.apache.spark.mllib.classification.{SVMModel, SVMWithSGD}
+import org.apache.spark.mllib.regression.LabeledPoint
+import net.fnothaft.gnocchi.transformations.GP2LabeledPoint
+
+
+trait BuildGnocchiModel {
+
+  def apply[T](rdd: RDD[GenotypeState],
+               phenotypes: RDD[Phenotype[T]]): GnocchiModel = {
+
+    // call RegressPhenotypes on the data
+    val assocs = fit(rdd, phenotypes)
+
+    // extract the model parameters (including p-value) for each variant and build LogisticGnocchiModel
+    val model = extractModel(assocs)
+
+    // save the LogisticGnocchiModel
+    model.save
+  }
+
+  def fit[T](rdd: RDD[GenotypeState],
+             phenotypes: RDD[Phenotype[T]]): RDD[Association]
+
+  def extractModel(assocs: RDD[Association]): GnocchiModel
+
+}
+
+trait AdditiveLogisticBuilder extends BuildGnocchiModel {
+
+  val regressionName = "Additive Logistic Regression with SGD"
+
+  def fit[T](rdd: RDD[GenotypeState],
+               phenotypes: RDD[Phenotype[T]]): RDD[Association] = {
+    AdditiveLogisticAssociation(rdd, phenotypes)
+  }
+
+  def extractModel(assocs: RDD[Association]): GnocchiModel = {
+
+  }
+}
+
+  object BuildDominantLogisticGnocchiModel extends BuildGnocchiModel with DominantLogistic {
+    val regressionName = "Dominant Logistic Regression with SGD"
+  }
+
+
+
+
