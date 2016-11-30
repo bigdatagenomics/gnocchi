86839049bdeafe58cb4bcf538751b7df3b1f7024
Nathaniel Parke <nathaniel.parke@gmail.com>
2016-11-30 14:44:10 -0800

    adds in additional ensemblers

--- gnocchi-core/src/main/scala/net/fnothaft/gnocchi/association/Ensembler.scala
+++ gnocchi-core/src/main/scala/net/fnothaft/gnocchi/association/Ensembler.scala
@@ -19,9 +19,11 @@ import net.fnothaft.gnocchi.models.Association
 
 object Ensembler extends Serializable {
 
-  def apply(ensembleMethod: String, snpArray: Array[(Double, Double, Association)]): (Double, Double) = {
+  def apply(ensembleMethod: String, snpArray: Array[(Double, Double, Association)], weights: Array[Double] = Array.empty[Double]): (Double, Double) = {
     ensembleMethod match {
       case "AVG" => average(snpArray)
+      case "MAX_PROB" => maxProb(snpArray)
+      case "W_AVG" => weightedAvg(snpArray, weights)
       case _     => average(snpArray) //still call avg until other methods implemented
     }
   }
@@ -30,6 +32,25 @@ object Ensembler extends Serializable {
     var sm = 0.0
     for (i <- snpArray.indices) {
       sm += snpArray(i)._1
+    }
+    (sm / snpArray.length, snpArray(0)._2)
+  }
+
+  def maxProb(snpArray: Array[(Double, Double, Association)]): (Double, Double) = {
+    var max = 0.0
+    for (i <- snpArray.indices) {
+      if ( snpArray(i)._1 > max ) {
+        max = snpArray(i)._1
+      }
+    }
+    (max, snpArray(0)._2)
+  }
+
+  def weightedAvg(snpArray: Array[(Double, Double, Association)], weights: Array[Double]): (Double, Double) = {
+    assert(snpArray.length == weights.length, "Array length mismatch in weighted average ensembler.")
+    var sm = 0.0
+    for (i <- snpArray.indices) {
+      sm += weights(i)*snpArray(i)._1
     }
     (sm / snpArray.length, snpArray(0)._2)
   }
