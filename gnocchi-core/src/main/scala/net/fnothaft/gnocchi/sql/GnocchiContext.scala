cc3bd14bb2bb5a966e5ea299a8034ad3807195c8
Nathaniel Parke <nathaniel.parke@gmail.com>
2017-08-14 15:10:44 -0700

    prints

--- gnocchi-core/src/main/scala/net/fnothaft/gnocchi/sql/GnocchiContext.scala
+++ gnocchi-core/src/main/scala/net/fnothaft/gnocchi/sql/GnocchiContext.scala
@@ -128,6 +128,9 @@ class GnocchiContext(@transient val sc: SparkContext) extends Serializable with 
 
     val genotypes = sparkSession.read.format("parquet").load(parquetInputDestination)
 
+    genotypes.collect().foreach(println)
+
+
     // transform the parquet-formatted genotypes into a dataFrame of GenotypeStates and convert to Dataset.
     val genotypeStates = toGenotypeStateDataFrame(genotypes, ploidy, sparse = false)
     // TODO: change convention so that generated variant name gets put in "names" rather than "contigName"
